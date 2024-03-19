f10f220d465ad094dd38ec28f9a3e4b8ef0c2cc6
Alban de Braquilanges <alban2bqs@gmail.com>
2024-03-19 09:49:10 +0100

    exercice 3

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,47 @@
-<script setup lang="ts">
+ <script setup lang="ts">
 const {find} = useStrapi()
+// import type {Player} from '~/models/player.model';
 
-const data = await find('players')
+// const data = await find('players')
+
+const { data, pending, error } = await useAsyncData('players', async () => { 
+        return await find('players', {
+            populate: '*',
+        }).then(res => res.data)
+    })
+
+
+
+    
+
 </script>
 
 <template>
     <div>
         <h1>Hello world</h1>    
-        <p>lalalala</p>
         <pre v-if="data">{{ data }}</pre>
+        lien cliquable avec slug
+        {{ data}}
+    
+
+
+        <template v-if="pending">
+            <p>Loading...</p>
+        </template>
+        <template v-else>
+            <div class="my-4">
+                <!-- <div v-for="player in players?.data" :key="player.slug">
+                    <a :href="`/players/${player.slug}`">{{ player.name }}</a>
+                </div> -->
+                <a :href="`/players/${player.slug}`" v-for="player in data" :key="player.slug">{{ player.name }}</a>
+            </div>
+        </template>
+
     </div>
 </template>
 
 <style scoped>
 
-</style>
+</style> 
+
+
---
+++ pages/players/[...slug].vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+
+const {findOne} = useStrapi()
+const route = useRoute()
+const {data, pending} = useAsyncData('player', 
+    () => findOne(`players/${route.params.slug}`)
+)
+</script>
+
+<template>
+    <h1>Youpi</h1>
+</template>
